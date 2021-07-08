2f46db814409969c65b3293480ecb5ded8d4e76d
Mabelys Arias <MabelysA@gmail.com>
2021-07-08 11:34:44 -0400

    adds instances and validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,5 @@
 class Meeting < ApplicationRecord
+  validates :title, :agenda, :location, :time, presence:true
   has_many :conferences
   has_many :speakers, through: :conferences
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,5 @@
 class Speaker < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
   has_many :conferences
   has_many :meetings, through: :conferences
 end
